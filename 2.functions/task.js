3484b936ac585692ed1fc9e9ec92681268476d99
Alex Makarov <rubenen@mail.ru>
2022-07-11 14:21:38 +0300

    function

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,39 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = Math.min(...arr);
+  max = Math.max(...arr);
+  sum = 0;
+  for (let number of arr) {
+    sum += number;
+  }
+  avg = +(sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let number of arr) {
+    sum += number;
+  }
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+function makeWork(arrOfArr, worker) {
+  let max = 0;
+  for (let arr of arrOfArr) {
+    if (max < worker(arr)) {
+      max = worker(arr);
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  const difference = max - min;
+  return Math.abs(difference);
 }
