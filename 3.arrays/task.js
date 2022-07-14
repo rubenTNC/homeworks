b6c3be92bbcc9ac70811742080a7d973465cbc3f
Alex Makarov <rubenen@mail.ru>
2022-07-14 06:28:27 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.every((item, index) => {
+    if (arr1.length !== arr2.length) {
+      return false;
+    }
+    if (item === arr2[index]) {
+      return true;
+    } else {
+      return false;
+    }
+  });
 
-  // Ваш код
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter((item) => item >= 0 && item % 3 === 0)
+    .map((item) => item * 10);
 
   return resultArr; // array
 }
