bd84fb1a113cc9f895067e3a54e78faf31247e63
Alex Makarov <rubenen@mail.ru>
2022-08-04 11:19:10 +0300

    async

---
+++ 7.async/task.js
@@ -0,0 +1,89 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId = null;
+    }
+    addClock(time, callback, id) {
+        if (!id) {
+            throw new Error('error text');
+        }
+        const indexId = this.alarmCollection.findIndex(item => item.id === id);
+        if (indexId !== -1) {
+            return console.error("");
+        } else {
+            this.alarmCollection.push({
+                id: id,
+                time: time,
+                callback: callback
+            });
+        }
+    }
+    removeClock(id) {
+        const indexId = this.alarmCollection.findIndex(item => item.id === id);
+        if (indexId === -1) {
+            return false;
+        } else {
+            this.alarmCollection.splice(indexId, 1);
+        }
+    }
+
+    getCurrentFormattedTime() {
+        return new Date().toLocaleTimeString().slice(0, -3);
+    }
+    start() {
+        function checkClock(call) {
+            if (call.time === this.getCurrentFormattedTime()) {
+                call.callback();
+            }
+            if (this.timerId === null) {
+                this.timerId = setInterval(() => {
+                    this.alarmCollection.forEach(clock => checkClock(call));
+                }, 1000);
+            }
+        }
+    }
+    stop() {
+        if(this.timerId !== null) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+    printAlarms() {
+        this.alarmCollection.forEach(item => console.log(`${item.id}, ${item.time}`));
+    }
+    clearAlarms() {
+        clearInterval(this.timerId);
+        this.alarmCollection.length = 0;
+    }
+}
+
+
+
+
+function testCase () {
+    let phoneAlarmClock = new AlarmClock();
+
+    phoneAlarmClock.addClock(phoneAlarmClock.getCurrentFormattedTime(), () => console.log("привет"), 1);
+
+    phoneAlarmClock.addClock(new Date(new Date().getTime() + 1 * 60000).toLocaleTimeString().slice(0, -3), () => {
+        console.log("привет+1");
+        phoneAlarmClock.removeClock(phoneAlarmClock.timerId)
+    }, 2);
+
+    phoneAlarmClock.addClock(new Date(new Date().getTime() + 2 * 60000).toLocaleTimeString().slice(0, -3), () => {
+        console.log("привет+2");
+        phoneAlarmClock.clearAlarms();
+        phoneAlarmClock.printAlarms();
+    }, 3);
+
+    phoneAlarmClock.printAlarms();
+
+    phoneAlarmClock.start();
+}
+
+
+
+
+testCase();
+
+
